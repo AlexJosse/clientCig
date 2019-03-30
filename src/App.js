e6fafdc4d0924248214d8bdb85dfca09f22fb222
josse alexandre <alexandre.josse@epitech.eu>
2019-03-30 22:26:44 +0100

    fix eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Dashboard } from './components/Dashboard/Dashboard.js';
-import { Login } from './components/Login/Login.js';
-import { Signup } from './components/Signup/Signup.js';
-import { Album } from './components/Home/Album.js';
-import { HeaderComponent } from './components/Home/Header.js';
-import { PrivateRoute } from './components/PrivateRoute.js';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard/Dashboard.js";
+import { Login } from "./components/Login/Login.js";
+import { Signup } from "./components/Signup/Signup.js";
+import { Album } from "./components/Home/Album.js";
+import { HeaderComponent } from "./components/Home/Header.js";
+import { PrivateRoute } from "./components/PrivateRoute.js";
+import "./App.css";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-content">
-                    <Switch>
-                        <Route exact path="/" component={Login}/>
-                        <Route exact path ="/signup" component={Signup}/>
-                        <Route exact path ="/album" component={Album}/>
-                        <Route exact path ="/header" component={HeaderComponent}/>
-                        <PrivateRoute path='/dashboard' component={Dashboard} />
-                    </Switch>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-content">
+					<Switch>
+						<Route exact path="/" component={Login}/>
+						<Route exact path ="/signup" component={Signup}/>
+						<Route exact path ="/album" component={Album}/>
+						<Route exact path ="/header" component={HeaderComponent}/>
+						<PrivateRoute path='/dashboard' component={Dashboard} />
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
